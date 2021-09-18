cf351ca898ad30d31dd513b8acad846483b7f08f
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-09-18 17:42:30 +0300

    Building Login page

--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from './components/Header';
 import styled from 'styled-components';
 import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {auth} from './firebase'
+import Login from './components/Login';
 
 function App() {
+  const [user, loading] = useAuthState(auth)
+
   return (
     <div className="App">
       <Router>
-        <>
+        {!user ? (
+          <Login />
+
+        ) : (<>
           <Header />
           <AppBody>
             <Sidebar />
             <Switch>
               <Route path="/" exact>
-                {/* Chat */}
+                <Chat />
               </Route>
             </Switch>
           </AppBody>
-        </>
+        </> ) }
+        
       </Router>
     </div>
   );
