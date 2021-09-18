9b3350017e0a6d9f77d8a111e7abce54b8b71abf
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-09-18 18:53:56 +0300

    Update loading spinner

--- src/App.js
+++ src/App.js
@@ -6,12 +6,28 @@ import styled from 'styled-components';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import {auth} from './firebase'
+import { auth } from './firebase'
 import Login from './components/Login';
-
+import Spinner from 'react-spinkit';
 function App() {
   const [user, loading] = useAuthState(auth)
 
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img src="http://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" alt="" />
+
+          <Spinner
+            name="ball-spin-fade-loader"
+            color="purple"
+            fadeIn="none"
+          />
+
+        </AppLoadingContents>
+      </AppLoading>
+    )
+  }
   return (
     <div className="App">
       <Router>
@@ -28,8 +44,8 @@ function App() {
               </Route>
             </Switch>
           </AppBody>
-        </> ) }
-        
+        </>)}
+
       </Router>
     </div>
   );
@@ -42,3 +58,25 @@ const AppBody = styled.div`
   display: flex;
   height: 100vh;
 `
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+  
+`
+
+const AppLoadingContents = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }`
